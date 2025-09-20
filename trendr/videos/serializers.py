6d5a492d5f2ef37eb38702e3746bac68ff9eef8e
riyaaaa123 <riyajindal769@gmail.com>
2025-09-21 05:22:59 +0530

    [feat] video app changes

--- trendr/videos/serializers.py
+++ trendr/videos/serializers.py
@@ -2,19 +2,10 @@ from rest_framework import serializers
 from .models import Video
 from product.serializers import ProductSerializer
 
-
 class VideoSerializer(serializers.ModelSerializer):
-    # Show product IDs in POST/PUT, and full product details in GET
-    products = serializers.PrimaryKeyRelatedField(queryset=ProductSerializer.Meta.model.objects.all(), many=True)
-
-    class Meta:
-        model = Video
-        fields = ["id", "uploader", "video_url", "caption", "likes", "created_at", "products"]
-
-
-class VideoDetailSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
+    uploader_name = serializers.CharField(source="uploader.name", read_only=True)
 
     class Meta:
         model = Video
-        fields = ["id", "uploader", "video_url", "caption", "likes", "created_at", "products"]
+        fields = ["id", "uploader", "video_url", "caption", "likes", "created_at", "products","uploader_name"]
