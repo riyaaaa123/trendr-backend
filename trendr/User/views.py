3b5cd3cf0a618f709b433b0efe5b3edf63d8a845
riyaaaa123 <riyajindal769@gmail.com>
2025-09-21 11:39:14 +0530

    allow any changes

--- trendr/User/views.py
+++ trendr/User/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
@@ -10,7 +11,9 @@ from .serializers import SignupSerializer, UserSerializer
 User = get_user_model()
 
 class SignupView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
+        
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
@@ -25,6 +28,7 @@ class SignupView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class LoginView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         email = request.data.get("email")
         password = request.data.get("password")
--- trendr/trendr/settings.py
+++ trendr/trendr/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,6 +91,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
 }
 
 # Password validation
@@ -131,6 +134,14 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATIC_URL = 'static/'
 # Custom user model
 AUTH_USER_MODEL = "User.User"
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),   
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
 
 
 # Default primary key field type
